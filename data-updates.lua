1c977e97b774412ee72fe76b2d112a7d5a718a3a
DeltaV <varady.tamas.m@gmail.com>
2026-04-13 17:20:02 +0100

    Remove logging

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,3 @@
-log("ttt_ssss")
 if mods["Krastorio2"] then
 	table.insert(data.raw["equipment-grid"]["gunship-equipment-grid"].equipment_categories, "kr-vehicle")
 	table.insert(data.raw["equipment-grid"]["gunship-equipment-grid"].equipment_categories, "kr-vehicle-motor")
@@ -9,11 +8,7 @@ if mods["Krastorio2"] then
 	table.insert(data.raw["equipment-grid"]["flying-fortress-equipment-grid"].equipment_categories, "kr-vehicle")
 	table.insert(data.raw["equipment-grid"]["flying-fortress-equipment-grid"].equipment_categories, "kr-vehicle-motor")
 	table.insert(data.raw["equipment-grid"]["flying-fortress-equipment-grid"].equipment_categories, "kr-vehicle-roboport")
-	log("ttt_k2 inside")
 	if mods["aai-industry"] then
-		log("ttt_aai industry inside")
-		log(serpent.block(data.raw["car"]["gunship"].energy_source))
-		log(serpent.block(data.raw["car"]["gunship"].energy_source.fuel_categories))
 		data.raw["car"]["gunship"].energy_source.fuel_categories = { "kr-vehicle-fuel" , "processed-chemical"}
 		data.raw["car"]["cargo-plane"].energy_source.fuel_categories = { "kr-vehicle-fuel" , "processed-chemical"}
 		data.raw["car"]["jet"].energy_source.fuel_categories =  { "kr-vehicle-fuel", "processed-chemical" }
